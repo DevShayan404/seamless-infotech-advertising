5720c2b6456d1748f59518000e7d5430fee6365b
MuhammadShayan404 <141303510+DevShayan404@users.noreply.github.com>
2024-08-05 14:54:02 +0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbAlertModule, NavbarComponent],
+  imports: [RouterOutlet, NgbAlertModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
