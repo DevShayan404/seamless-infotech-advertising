6eb52ea8b2f6f6e6497194fa69d2c734c0301d53
MuhammadShayan404 <141303510+DevShayan404@users.noreply.github.com>
2024-08-08 14:42:55 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-const MINIMUM_LOADING_TIME = 2000; // Minimum loader display time in milliseconds
-const startTime = Date.now();
-
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err))
   .then(() => {
     const body = document.querySelector('body');
     if (body) {
-      window.addEventListener('load', () => {
-        const elapsedTime = Date.now() - startTime;
-        const remainingTime = MINIMUM_LOADING_TIME - elapsedTime;
-        
-        setTimeout(() => {
-          body.classList.add('app-loaded');
-        }, Math.max(remainingTime, 0));
-      });
+      body.classList.add('app-loaded');
     }
   });
